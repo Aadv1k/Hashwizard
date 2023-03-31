e242914bdf1bb9c6ca47e19212072b1fe27069d2
Aadv1k <aadv1k@outlook.com>
2023-03-31 12:46:57 +0530

    Add arg support in index

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const server = require("./server");
 const DumpData = require("./lib/DumpRemoteTableToFile.js");
 const { crackAlgorithms } = require("./common/const.js");
 
+const ARGS = process.argv.filter(e => e.startsWith("--")).map(e => e.slice(2).split('='));
 (async () => {
-  await DumpData(crackAlgorithms)
+  let argPort = ARGS.find(e => e[0] === "port")?.[1] ?? PORT;
+  let argDump = !Boolean(ARGS.find(e => e[0] === "no-dump"));
+  
+  if (argDump) {
+    await DumpData(crackAlgorithms)
+  }
+
   server.listen(PORT, () => {
-    console.log(`server listening at http://localhost:${PORT}`)
+    console.log(`server listening at http://localhost:${argPort}`)
   })
 })()
 
